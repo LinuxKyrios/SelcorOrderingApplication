8d79eacf34c7fc2b45675612521c26c86cc502c7
Linux Kyrios <malinowskim92@protonmail.com>
2020-11-23 22:32:15 +0100

    Initial commit

---
+++ app/src/main/java/com/linuxkyrios/selcororderingapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.linuxkyrios.selcororderingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
