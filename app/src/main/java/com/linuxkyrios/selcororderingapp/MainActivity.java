d6290decc875c953413c5f0e38a7620292bfe31e
Linux Kyrios <malinowskim92@protonmail.com>
2020-11-24 00:11:08 +0100

    Created and applied custom toolbar and added specific activity in Main Activity.

--- app/src/main/java/com/linuxkyrios/selcororderingapp/MainActivity.java
+++ app/src/main/java/com/linuxkyrios/selcororderingapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.linuxkyrios.selcororderingapp;
 
+import androidx.appcompat.widget.Toolbar; //Necessary class to use toolbar in app
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Downloading reference to toolbar, and setting it as activity bar
+        getSupportActionBar().hide();
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
